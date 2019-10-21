73dcba1291382a326e4e54d5ee466273dd286eb9
Thiago Romanelli <thiagoo.romanelli@gmail.com>
2019-10-20 23:01:33 -0300

    initial commit

---
+++ products.py
@@ -0,0 +1,83 @@
+from datetime import datetime
+from flask import jsonify, make_response, abort
+
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/") # Local
+db = client.products
+
+def get_dict_from_mongodb():
+    itens_db = db.products.find()
+    PRODUCTS = {}
+    for i in itens_db:
+            i.pop('_id') # retira id: criado automaticamente 
+            item = dict(i)
+            PRODUCTS[item["product_name"]] = (i)
+    return PRODUCTS
+
+def get_timestamp():
+    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+
+def read_all_products():
+    PRODUCTS = get_dict_from_mongodb()
+    dict_products = [PRODUCTS[key] for key in sorted(PRODUCTS.keys())]
+    products = jsonify(dict_products)
+    qtd = len(dict_products)
+    content_range = "products 0-"+str(qtd)+"/"+str(qtd)
+    # Configura headers
+    products.headers['Access-Control-Allow-Origin'] = '*'
+    products.headers['Access-Control-Expose-Headers'] = 'Content-Range'
+    products.headers['Content-Range'] = content_range
+    return products
+
+def create_new_product(product):
+    product_name = product.get("product_name", None)
+    product_type = product.get("product_type", None)
+    points = product.get("points", None)
+    PRODUCTS = get_dict_from_mongodb()
+    if product_name not in PRODUCTS and product_name is not None:
+        item = {
+            "product_name": product_name,
+            "product_type": product_type,
+            "points": points,
+            "timestamp": get_timestamp(),
+        }
+        db.products.insert_one(item)
+        return make_response(
+            "Produto '{product_name}' criado com sucesso".format(product_name=product_name), 201
+        )
+    else:
+        abort(
+            406,
+            "Produto ja existente"
+        )
+
+def update_product(product_name, points_to_update):
+    query = { "product_name": product_name }
+    update = { "$set": {
+            "points": points_to_update.get("points_to_update"),
+            "timestamp": get_timestamp(), } 
+        }
+    PRODUCTS = get_dict_from_mongodb()
+
+    if product_name in PRODUCTS:
+        db.products.update_one(query, update)
+        PRODUCTS = get_dict_from_mongodb()
+        return PRODUCTS[product_name]
+    else:
+        abort(
+            404, "Produto '{product_name}' nao encontrado".format(product_name=product_name)
+        )
+
+def delete(product_name):
+    query = { "product_name": product_name }
+    PRODUCTS = get_dict_from_mongodb()
+    if product_name in PRODUCTS:
+        db.products.delete_one(query)
+        return make_response(
+            "{product_name} deletado com sucesso".format(product_name=product_name), 200
+        )
+    else:
+        abort(
+            404, "Usuario '{product_name}' nao encontrado".format(product_name=product_name)
+        )
---
+++ users.py
@@ -0,0 +1,117 @@
+from datetime import datetime
+from flask import jsonify, make_response, abort
+
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/") # Local
+db = client.usuarios
+
+def get_dict_from_mongodb():
+    itens_db = db.usuarios.find()
+    PEOPLE = {}
+    for i in itens_db:
+            i.pop('_id') # retira id: criado automaticamente 
+            item = dict(i)
+            PEOPLE[item["login"]] = (i)
+    return PEOPLE
+
+def get_timestamp():
+    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+
+def read_all():
+    PEOPLE = get_dict_from_mongodb()
+    dict_usuarios = [PEOPLE[key] for key in sorted(PEOPLE.keys())]
+    usuarios = jsonify(dict_usuarios)
+    qtd = len(dict_usuarios)
+    content_range = "usuarios 0-"+str(qtd)+"/"+str(qtd)
+    # Configura headers
+    usuarios.headers['Access-Control-Allow-Origin'] = '*'
+    usuarios.headers['Access-Control-Expose-Headers'] = 'Content-Range'
+    usuarios.headers['Content-Range'] = content_range
+    return usuarios
+
+def read_one(login):
+    PEOPLE = get_dict_from_mongodb()
+    if login in PEOPLE:
+        users = PEOPLE.get(login)
+    else:
+        abort(
+            404, "Login {login} nao encontrado".format(login=login)
+        )
+    return users
+
+
+def create(users):
+    login = users.get("login", None)
+    password = users.get("password", None)
+    PEOPLE = get_dict_from_mongodb()
+    if login not in PEOPLE and login is not None and password not in PEOPLE:
+        item = {
+            "login": login,
+            "password": password,
+            "points": 0,
+            "timestamp": get_timestamp(),
+        }
+        db.usuarios.insert_one(item)
+        return make_response(
+            "'{login}' criado com sucesso".format(login=login), 201
+        )
+    else:
+        abort(
+            406,
+            "Login ou password ja existente"
+        )
+
+
+def update_pwd(login, new_pwd):
+    query = { "login": login }
+    update = { "$set": {
+            "password": new_pwd.get("new_pwd"),
+            "timestamp": get_timestamp(), } 
+        }
+    PEOPLE = get_dict_from_mongodb()
+
+    if login in PEOPLE:
+        db.usuarios.update_one(query, update)
+        PEOPLE = get_dict_from_mongodb()
+        return PEOPLE[login]
+    else:
+        abort(
+            404, "Usuario '{login}' nao encontrado".format(login=login)
+        )
+
+
+def update_points(login, points_to_update):
+    query = { "login": login }
+    update = { 
+        "$set":{
+            "timestamp": get_timestamp(),
+        },
+        "$inc":{
+            "points": points_to_update.get("points_to_update")
+        }             
+    }
+
+    PEOPLE = get_dict_from_mongodb()
+
+    if login in PEOPLE:
+        db.usuarios.update_one(query, update)
+        PEOPLE = get_dict_from_mongodb()
+        return PEOPLE[login]
+    else:
+        abort(
+            404, "Usuario '{login}' nao encontrado".format(login=login)
+        )
+
+def delete(login):
+    query = { "login": login }
+    PEOPLE = get_dict_from_mongodb()
+    if login in PEOPLE:
+        db.usuarios.delete_one(query)
+        return make_response(
+            "{login} deletado com sucesso".format(login=login), 200
+        )
+    else:
+        abort(
+            404, "Usuario '{login}' nao encontrado".format(login=login)
+        )
