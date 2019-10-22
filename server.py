ff0a4e7f2df89a4afcbc058119094a92975e91f8
Murillo Cuervo <murillocuervot@gmail.com>
2019-10-21 21:55:25 -0300

    add front

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ CORS(app.app,resources=r'/api/*',methods=['GET', 'POST', 'OPTIONS', 'PUT', 'DELE
 
 @app.route('/')
 def home():
-    return render_template('clientes.html')
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
